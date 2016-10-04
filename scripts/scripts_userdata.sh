533d95e6a329a0ab27c29d10e7433ce54bdf1de4
Mcgill <erinmc@f45c89b0babb.ant.amazon.com>
2016-10-04 14:32:04 -0400

    added flag to exit script upon error

--- scripts/scripts_userdata.sh
+++ scripts/scripts_userdata.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 #UserData and or scripts should be stored here, but only for source code revision purposes and CloudFormation templates should always refer to 'quickstart-reference' S3 bucket
 #
 # Configuring the GitHub Enterprise server
