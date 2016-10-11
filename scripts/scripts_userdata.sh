98661b3ac42afcb388ac319a3f36f33f8676cf11
Mcgill <erinmc@f45c89b0babb.ant.amazon.com>
2016-10-11 13:47:01 -0400

    correct to right variable

--- scripts/scripts_userdata.sh
+++ scripts/scripts_userdata.sh
@@ -5,12 +5,12 @@
 
 DATE=`date +%d-%m-%Y`
 date >/root/install_date
-echo "Installing Tools"
+#echo "Installing Tools"
 # Install tools needed for bootstraping
-easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
-pip install awscli
+#easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+#wget https://bootstrap.pypa.io/get-pip.py
+#sudo python get-pip.py
+#pip install awscli
 
 ###########################################
 #  Configure the GitHub Enterprise server
@@ -24,26 +24,26 @@ AWS_CMD='/usr/local/bin/aws'
 EC2_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
 
 # Copy down the license file from the S3 Bucket
-AWS_CMD s3 cp s3://${GHE_S3_BUCKET}/${GHE_LICENSE} /tmp
+${AWS_CMD} s3 cp s3://${GHE_S3_BUCKET}/${GHE_LICENSE} /tmp
 sleep 25
 
 #Upload the license and set the GitHub Enterprise Admin password
 curl -i -k -L -F license=@/tmp/${GHE_LICENSE} -F password=${GHE_ADMIN_PASSWD} -X POST https://${EC2_IP}:8443/setup/api/start 
 
 # Initiate the configuration process
-curl -i -k -L -X POST https://api_key:${ANSIBLE_ADMIN_PASSWD}@localhost:8443/setup/api/configure
+curl -i -k -L -X POST https://api_key:${GHE_ADMIN_PASSWD}@localhost:8443/setup/api/configure
 
 # Get the default settings
-curl -i -k -L https://api_key:${ANSIBLE_ADMIN_PASSWD}@localhost:8443/setup/api/settings
+curl -i -k -L https://api_key:${GHE_ADMIN_PASSWD}@localhost:8443/setup/api/settings
 
 # Check the configuration status and continue to check until the configuration is complete
-CONFIG_STATUS=`curl -k -L https://api_key:${ANSIBLE_ADMIN_PASSWD}@localhost:8443/setup/api/configcheck | awk -F, '{print $NF}' | awk -F: '{print $NF}' |tail -n1 `
+CONFIG_STATUS=`curl -k -L https://api_key:${GHE_ADMIN_PASSWD}@localhost:8443/setup/api/configcheck | awk -F, '{print $NF}' | awk -F: '{print $NF}' |tail -n1 `
 while [[ ${CONFIG_STATUS} != *'DONE'* ]]; do
   sleep 2
   echo date
   echo 'Waiting for config status to contain done for Reloading application services'
   echo ${CONFIG_STATUS}
-  CONFIG_STATUS=`curl -k -L https://api_key:${ANSIBLE_ADMIN_PASSWD}@localhost:8443/setup/api/configcheck | awk -F, '{print $NF}' | awk -F: '{print $NF}' |tail -n1 `
+  CONFIG_STATUS=`curl -k -L https://api_key:${GHE_ADMIN_PASSWD}@localhost:8443/setup/api/configcheck | awk -F, '{print $NF}' | awk -F: '{print $NF}' |tail -n1 `
 done
 rm -f ${ADMININFO} 
 
