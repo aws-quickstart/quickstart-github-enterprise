e456a0ff7a0e446ec4fe2673e37ab0da01d23152
Mcgill <erinmc@f45c89b0babb.ant.amazon.com>
2016-10-17 17:10:57 -0400

    changed to set the console password

--- scripts/scripts_userdata.sh
+++ scripts/scripts_userdata.sh
@@ -5,19 +5,13 @@
 
 DATE=`date +%d-%m-%Y`
 date >/root/install_date
-#echo "Installing Tools"
-# Install tools needed for bootstraping
-#easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
-#wget https://bootstrap.pypa.io/get-pip.py
-#sudo python get-pip.py
-#pip install awscli
 
 ###########################################
 #  Configure the GitHub Enterprise server
 ###########################################
 
 ADMININFO='/etc/gheadmin.conf'
-GHE_ADMIN_PASSWD=`cat $ADMININFO| grep github_admin_password | awk -F: '{print $2}'`
+GHE_CONSOLE_PASSWD=`cat $ADMININFO| grep github_console_password | awk -F: '{print $2}'`
 GHE_S3_BUCKET=`cat $ADMININFO| grep github_s3_bucket | awk -F: '{print $2}'`
 GHE_LICENSE=`cat $ADMININFO| grep github_license_file | awk -F: '{print $2}'`
 AWS_CMD='/usr/local/bin/aws'
@@ -28,23 +22,20 @@ ${AWS_CMD} s3 cp s3://${GHE_S3_BUCKET}/${GHE_LICENSE} /tmp
 sleep 25
 
 #Upload the license and set the GitHub Enterprise Admin password
-curl -i -k -L -F license=@/tmp/${GHE_LICENSE} -F password=${GHE_ADMIN_PASSWD} -X POST https://${EC2_IP}:8443/setup/api/start 
+curl -i -k -L -F license=@/tmp/${GHE_LICENSE} -F password=${GHE_CONSOLE_PASSWD} -X POST https://${EC2_IP}:8443/setup/api/start 
 
 # Initiate the configuration process
-curl -i -k -L -X POST https://api_key:${GHE_ADMIN_PASSWD}@localhost:8443/setup/api/configure
+curl -i -k -L -X POST https://api_key:${GHE_CONSOLE_PASSWD}@localhost:8443/setup/api/configure
 
 # Get the default settings
-curl -i -k -L https://api_key:${GHE_ADMIN_PASSWD}@localhost:8443/setup/api/settings
+curl -i -k -L https://api_key:${GHE_CONSOLE_PASSWD}@localhost:8443/setup/api/settings
 
 # Check the configuration status and continue to check until the configuration is complete
-CONFIG_STATUS=`curl -k -L https://api_key:${GHE_ADMIN_PASSWD}@localhost:8443/setup/api/configcheck | awk -F, '{print $NF}' | awk -F: '{print $NF}' |tail -n1 `
+CONFIG_STATUS=`curl -k -L https://api_key:${GHE_CONSOLE_PASSWD}@localhost:8443/setup/api/configcheck | awk -F, '{print $NF}' | awk -F: '{print $NF}' |tail -n1 `
 while [[ ${CONFIG_STATUS} != *'DONE'* ]]; do
   sleep 2
   echo date
   echo 'Waiting for config status to contain done for Reloading application services'
   echo ${CONFIG_STATUS}
-  CONFIG_STATUS=`curl -k -L https://api_key:${GHE_ADMIN_PASSWD}@localhost:8443/setup/api/configcheck | awk -F, '{print $NF}' | awk -F: '{print $NF}' |tail -n1 `
+  CONFIG_STATUS=`curl -k -L https://api_key:${GHE_CONSOLE_PASSWD}@localhost:8443/setup/api/configcheck | awk -F, '{print $NF}' | awk -F: '{print $NF}' |tail -n1 `
 done
-rm -f ${ADMININFO} 
-
-echo "Finished AWSQuickStart Bootstraping"
