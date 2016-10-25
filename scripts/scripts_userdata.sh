cdab2a782dbffaa6d7e8c2f657091cfa3e8142ea
Mcgill <erinmc@f45c89b0babb.ant.amazon.com>
2016-10-25 17:21:23 -0400

    Pass config settings as arguments so they are not reflected in user data

--- scripts/scripts_userdata.sh
+++ scripts/scripts_userdata.sh
@@ -10,32 +10,34 @@ date >/root/install_date
 #  Configure the GitHub Enterprise server
 ###########################################
 
-ADMININFO='/etc/gheadmin.conf'
-GHE_CONSOLE_PASSWD=`cat $ADMININFO| grep github_console_password | awk -F: '{print $2}'`
-GHE_S3_BUCKET=`cat $ADMININFO| grep github_s3_bucket | awk -F: '{print $2}'`
-GHE_LICENSE=`cat $ADMININFO| grep github_license_file | awk -F: '{print $2}'`
 AWS_CMD='/usr/local/bin/aws'
 EC2_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
 
+# ARGS for script
+# 1: GHE_CONSOLE_PASSWORD
+# 2: GHE_S3_BUCKET
+# 3: GHE_LICENSE
+
+
 # Copy down the license file from the S3 Bucket
-${AWS_CMD} s3 cp s3://${GHE_S3_BUCKET}/${GHE_LICENSE} /tmp
+${AWS_CMD} s3 cp s3://$2/$3 /tmp
 sleep 25
 
 #Upload the license and set the GitHub Enterprise Admin password
-curl -i -k -L -F license=@/tmp/${GHE_LICENSE} -F password=${GHE_CONSOLE_PASSWD} -X POST https://${EC2_IP}:8443/setup/api/start 
+curl -i -k -L -F license=@/tmp/$3 -F password=$1 -X POST https://${EC2_IP}:8443/setup/api/start 
 
 # Initiate the configuration process
-curl -i -k -L -X POST https://api_key:${GHE_CONSOLE_PASSWD}@localhost:8443/setup/api/configure
+curl -i -k -L -X POST https://api_key:$1@localhost:8443/setup/api/configure
 
 # Get the default settings
-curl -i -k -L https://api_key:${GHE_CONSOLE_PASSWD}@localhost:8443/setup/api/settings
+curl -i -k -L https://api_key:$1@localhost:8443/setup/api/settings
 
 # Check the configuration status and continue to check until the configuration is complete
-CONFIG_STATUS=`curl -k -L https://api_key:${GHE_CONSOLE_PASSWD}@localhost:8443/setup/api/configcheck | awk -F, '{print $NF}' | awk -F: '{print $NF}' |tail -n1 `
+CONFIG_STATUS=`curl -k -L https://api_key:$1@localhost:8443/setup/api/configcheck | awk -F, '{print $NF}' | awk -F: '{print $NF}' |tail -n1 `
 while [[ ${CONFIG_STATUS} != *'DONE'* ]]; do
   sleep 2
   echo date
   echo 'Waiting for config status to contain done for Reloading application services'
   echo ${CONFIG_STATUS}
-  CONFIG_STATUS=`curl -k -L https://api_key:${GHE_CONSOLE_PASSWD}@localhost:8443/setup/api/configcheck | awk -F, '{print $NF}' | awk -F: '{print $NF}' |tail -n1 `
+  CONFIG_STATUS=`curl -k -L https://api_key:$1@localhost:8443/setup/api/configcheck | awk -F, '{print $NF}' | awk -F: '{print $NF}' |tail -n1 `
 done
