57830a150ccb9fc384610b6befa11eae34472ff3
Mcgill <erinmc@f45c89b0babb.ant.amazon.com>
2016-10-25 17:22:26 -0400

    Passing arguments instead of setting variables

--- scripts/adminuser_org_repo.sh
+++ scripts/adminuser_org_repo.sh
@@ -1,23 +1,23 @@
 #!/bin/bash -ex
 
-ADMININFO='/etc/gheadmin.conf'
-GHE_ADMINUSER_NAME=`cat $ADMININFO| grep github_adminuser_name | awk -F':' '{print $2}'`
-GHE_ADMINUSER_EMAIL=`cat $ADMININFO| grep github_adminuser_email | awk -F':' '{print $2}'`
-GHE_ADMINUSER_PASSWD=`cat $ADMININFO| grep github_adminuser_password | awk -F':' '{print $2}'`
-GHE_ORG=`cat $ADMININFO| grep github_organization | awk -F':' '{print $2}'`
-GHE_REPO=`cat $ADMININFO| grep github_repository | awk -F':' '{print $2}'`
+# ARGS for script
+# 1: GHE_ADMINUSER_NAME
+# 2: GHE_ADMINUSER_EMAIL
+# 3: GHE_ADMINUSER_PASSWD
+# 4: GHE_ORG
+# 5: GHE_REPO
 
-ORG=\"${GHE_ORG}\"
-ADMIN_USER=\"${GHE_ADMINUSER_NAME}\"
+ORG=\"$4\"
+ADMIN_USER=\"$1\"
 
 EC2_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
 
 cat <<-EOF | ghe-console
   exit 1 if !GitHub.enterprise_first_run?
 
-  USER = "${GHE_ADMINUSER_NAME}"
-  EMAIL = "${GHE_ADMINUSER_EMAIL}"
-  PASSWD = "${GHE_ADMINUSER_PASSWD}"
+  USER = "$1"
+  EMAIL = "$2"
+  PASSWD = "$3"
 
   user = User.create_with_random_password USER
   user.email = EMAIL
@@ -27,15 +27,14 @@ cat <<-EOF | ghe-console
   end
 EOF
 
-#rm -f ${ADMININFO}
-
-MAKE_ORG=`curl -i -k -L -H "Content-Type: application/json" --write-out '%{http_code}' -d "{\"login\": ${ORG}, \"admin\": ${ADMIN_USER}}" -X POST https://${GHE_ADMINUSER_NAME}:${GHE_ADMINUSER_PASSWD}@${EC2_IP}/api/v3/admin/organizations`
+MAKE_ORG=`curl -i -k -L -H "Content-Type: application/json" --write-out '%{http_code}' -d "{\"login\": ${ORG}, \"admin\": ${ADMIN_USER}}" -X POST https://$1:$3@${EC2_IP}/api/v3/admin/organizations`
 
 echo ${MAKE_ORG}
 
-MAKE_REPO=`curl -i -k -L -H "Content-Type: application/json" -d "{\"name\": \"${GHE_REPO}\", \"private\": \"true\", \"auto_init\": \"true\"}" -X POST https://${GHE_ADMINUSER_NAME}:${GHE_ADMINUSER_PASSWD}@${EC2_IP}/api/v3/orgs/${GHE_ORG}/repos`
+MAKE_REPO=`curl -i -k -L -H "Content-Type: application/json" -d "{\"name\": \"$5\", \"private\": \"true\", \"auto_init\": \"true\"}" -X POST https://$1:$3@${EC2_IP}/api/v3/orgs/$4/repos`
 # The above is supposed to return a 201
 echo "The above is supposed to return to 201"
 echo ${MAKE_REPO}
 
+#rm -f ${ADMININFO}
 echo "Finished AWSQuickStart Bootstraping"
