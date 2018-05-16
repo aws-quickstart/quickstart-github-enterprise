97c424de42985aeb853e0098bc29dfaf0e64e514
Christian Weber <webdog@github.com>
2018-05-15 20:11:55 -0500

    works with org creator and repo creator integrated

--- scripts/adminuser_org_repo.sh
+++ scripts/adminuser_org_repo.sh
@@ -12,7 +12,8 @@
 ORG=\"$4\"
 ADMIN_USER=\"$1\"
 
-EC2_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+#EC2_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+EC2_IP='54.242.94.104'
 
 ##########################################
 # Check status function
@@ -27,6 +28,40 @@ else
 fi
 }
 
+#Create the first GHE User/Admin Account
+#TEMPDIR=`mktemp -d /tmp/XXXXXXXXXXXXX`
+#curl -iskL https://$EC2_IP/join | grep 'Status: 200 OK'
+#curl -k -v -L -c $TEMPDIR/cookies https://$EC2_IP/login > $TEMPDIR/github-curl.out
+#authenticity_token=$(grep 'name="authenticity_token"' $TEMPDIR/github-curl.out | head -1 | sed -e 's/.*value=\([^"]*\)".*/\1/')
+#curl -X POST -k -v -b $TEMPDIR/cookies -c -L $TEMPDIR/cookies \
+#	-F "authenticity_token=$authenticity_token" \
+#	=F "user[login]=$1" \
+#	-F "user[email]=$2" \
+#	-F "user[password=$3" \
+#	-F "user[password_confirmation]=$3" \
+#	-F "source_label=Detail Form" \
+#	https://$EC2_IP/join > $TEMPDIR/github-curl.out 2>&1
+#cat $TEMPDIR/github-curl.out
+#grep "< Set Cookie: logged_in=yes;" $TEMPDIR/github-curl.out
+#rm -rf $TEMPDIR
+
+  echo "Creating first GitHub Enterprise administrator account"
+  TEMPDIR=`mktemp -d /tmp/XXXXXXXXXXXXX`
+  echo $TEMPDIR
+  curl -iskL https://$EC2_IP/join | grep 'Status: 200 OK'
+  curl -k -v -L -c $TEMPDIR/cookies https://$EC2_IP/login > $TEMPDIR/github-curl.out
+    authenticity_token=$(grep 'name="authenticity_token"' $TEMPDIR/github-curl.out | head -1 | sed -e 's/.*value="\([^"]*\)".*/\1/')
+    curl -X POST -k -v -b $TEMPDIR/cookies -c -L $TEMPDIR/cookies \
+    -F "authenticity_token=$authenticity_token" \
+    -F "user[login]=$1" \
+    -F "user[email]=$2" \
+    -F "user[password]=$3" \
+    -F "user[password_confirmation]=$3" \
+    -F "source_label=Detail Form" \
+    https://$EC2_IP/join >$TEMPDIR/github-curl.out 2>&1
+    cat $TEMPDIR/github-curl.out
+    grep "< Set-Cookie: logged_in=yes;" $TEMPDIR/github-curl.out
+    rm -rf $TEMPDIR
 
 MAKE_ORG=$(curl -i -k -L -H "Content-Type: application/json" --write-out '%{http_code}' --silent -d "{\"login\": ${ORG}, \"admin\": ${ADMIN_USER}}" -X POST https://$1:$3@${EC2_IP}/api/v3/admin/organizations)
 
